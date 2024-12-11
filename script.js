cbc51817b8b700d125c809ca01f0de75bb0d7551
JuneMoeSat <125462387+JuneMoeSat@users.noreply.github.com>
2024-12-11 10:00:21 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".team-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+ const videoCards = document.querySelectorAll('.video-card');
+            const modal = document.getElementById('videoModal');
+            const modalVideo = document.getElementById('modal-video');
+            const modalTitle = document.getElementById('videoModalLabel');
+            const modalDescription = document.getElementById('modal-description');
+
+            videoCards.forEach(card => {
+                card.addEventListener('click', () => {
+                    const videoUrl = card.getAttribute('data-video');
+                    const title = card.getAttribute('data-title');
+                    const description = card.getAttribute('data-description');
+
+                    modalVideo.src = videoUrl;
+                    modalTitle.innerText = title;
+                    modalDescription.innerHTML = description;
+                });
+            });
+
+            modal.addEventListener('hidden.bs.modal', () => {
+                modalVideo.src = '';
+            });
+
+
+            let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+    setPositionThumbnail();
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+function setPositionThumbnail () {
+    let thumbnailActive = document.querySelector('.thumbnail .item.active');
+    let rect = thumbnailActive.getBoundingClientRect();
+    if (rect.left < 0 || rect.right > window.innerWidth) {
+        thumbnailActive.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+    }
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
